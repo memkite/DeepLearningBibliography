d69e479527a81a3263ce37a6b957f499d2869956
Amund Tveit <atveit@gmail.com>
2014-10-14 16:00:39 +0200

    preliminary processing of rawdata

---
+++ miscdata/process_rawdata.py
@@ -0,0 +1,53 @@
+import codecs
+fh = codecs.open('rawdata.txt', encoding="utf-8")
+
+title_next=False
+author_next=False
+abstract_next=False
+
+raw_title = u""
+raw_authors = u""
+raw_abstract = u""
+
+for line in fh:
+    if line.startswith("\n"):
+#        print "#", line.strip()
+        abstract_next=False 
+        author_next = False
+        title_next = True
+        raw_title = u""
+        raw_authors = u""
+        raw_abstract = u""
+        continue
+
+    if title_next:
+        title_next = False
+        raw_title = line.strip()
+        author_next = True
+        continue
+
+    if author_next:
+        author_next=False
+        raw_authors = line.strip()
+        continue
+        
+    if abstract_next:
+        raw_abstract = raw_abstract + " " + line.strip()
+        continue
+
+    if len(raw_authors) > 0 and len(raw_title) > 0:
+        raw_title = raw_title.replace("[PDF]","").strip()
+        raw_title = raw_title.replace("[DOC]", "").strip()
+        raw_title = raw_title.replace("[HTML]", "").strip()
+
+        raw_authors = raw_authors.split(" - ")[0].strip()
+
+        print "@misc{" # + id
+        print '  "title": "%s",' % (raw_title.replace('"'," "))
+        print '  "author": "%s"' % (raw_authors.replace('"'," "))
+        print '  "abstract": "%s"' % (raw_abstract.replace('"'," "))
+        print '}'
+        raw_title = u""
+        raw_authors = u""
+        raw_abstract = u""
+    
